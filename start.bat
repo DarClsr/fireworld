b1d2729856e5cfba19c38d3da25293b8605de898
iwan <15735047414@163.com>
2026-08-27 15:24:45 +0800

    chore: 双击试玩入口 start.bat

---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+rem 衔烛 M0 灰盒试玩：WASD 移动 / 空格跳 / 左键轻击三连 / 右键蓄力重击 / E 加星籽 / ESC 放鼠标
+"%LOCALAPPDATA%\Microsoft\WinGet\Packages\GodotEngine.GodotEngine_Microsoft.Winget.Source_8wekyb3d8bbwe\Godot_v4.7.1-stable_win64.exe" --path game --resolution 1280x720
